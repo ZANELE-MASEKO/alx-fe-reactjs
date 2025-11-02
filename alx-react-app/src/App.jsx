df699278f9b489219e16edf1e182dd3aef1d8c71
zanele maseko <itszanele@gmail.com>
2025-11-02 15:44:10 +0200

    userprofile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -36,7 +37,9 @@ function App() {
 
       <Header />,
       <MainContent />,
-      <Footer />.
+      <Footer />
+
+      <UserProfile name='Alice' age='25' bio='Loves hiking and photography' />
     </>
   )
 }
