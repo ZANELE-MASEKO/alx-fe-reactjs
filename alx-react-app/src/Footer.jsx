c7e42a67ae5244b29f140fd6961143f650a55e26
zanele maseko <itszanele@gmail.com>
2025-11-02 16:03:46 +0200

    component changes

--- alx-react-app/src/Footer.jsx
+++ alx-react-app/src/Footer.jsx
@@ -1,9 +1,7 @@
-function Footer() {
+export default function Footer() {
      return (
         <footer>
             <p>&copy; 2023 City Lovers</p>
         </footer>
      )
 }
-
-export default Footer
--- alx-react-app/src/Header.jsx
+++ alx-react-app/src/Header.jsx
@@ -1,9 +1,7 @@
-function Header() {
+export default function Header() {
      return (
         <header>
             <h1>My Favourite Cities</h1>
         </header>
      );
 }
-
-export default Header
