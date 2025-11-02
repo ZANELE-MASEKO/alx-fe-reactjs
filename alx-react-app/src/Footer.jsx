fb264304af21268aee4dc4e3641f71f5ff71ca2b
zanele maseko <itszanele@gmail.com>
2025-11-02 15:50:37 +0200

    spelling

--- alx-react-app/src/Footer.jsx
+++ alx-react-app/src/Footer.jsx
@@ -1,9 +1,7 @@
 function Footer() {
      return (
         <footer>
-            <p>
-                &copy; 2023 City Lovers
-            </p>
+            <p>&copy; 2023 City Lovers</p>
         </footer>
      )
 }
