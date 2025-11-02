78174544fc013dcfd54dc7e621ca8c1ee0c259f2
zanele maseko <itszanele@gmail.com>
2025-11-02 15:33:23 +0200

    app.jsx changes

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -34,9 +34,9 @@ function App() {
       </p>
       <WelcomeMessage />
 
-      <Header />
-      <MainContent />
-      <Footer />
+      <Header />,
+      <MainContent />,
+      <Footer />.
     </>
   )
 }
