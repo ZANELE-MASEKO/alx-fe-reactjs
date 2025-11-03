943186638afed59d135a36b5e29579a7fadfa205
zanele maseko <itszanele@gmail.com>
2025-11-03 10:36:11 +0200

    app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -34,7 +34,9 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <WelcomeMessage />
-      <Header />, <MainContent />, <Footer />
+      <Header /> 
+      <MainContent /> 
+      <Footer />
 
       <UserProfile name='Alice' age='25' bio='Loves hiking and photography' />
     </>
