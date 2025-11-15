61e6fbc32f806c8992bca87e32e2cacdc7eab862
zanele maseko <itszanele@gmail.com>
2025-11-15 14:50:43 +0200

    more chnages

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,16 +1,19 @@
-// DeleteRecipeButton.jsx
 import { useRecipeStore } from './recipeStore';
+import { useNavigate } from 'react-router-dom'; 
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate(); 
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
-    alert("Recipe deleted!");
+
+    
+    navigate('/'); 
   };
 
   return (
-    <button onClick={handleDelete} style={{ color: "red" }}>
+    <button onClick={handleDelete} style={{ color: 'red' }}>
       Delete Recipe
     </button>
   );
