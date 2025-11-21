928e08d5f5cada30f5048263ea64028eceac05a5
zanele maseko <itszanele@gmail.com>
2025-11-21 16:45:56 +0200

    search bar component

---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,14 @@
+import {UseState} from 'react'
+
+const Search = () => {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="">Enter Username:</label>
+        <input type="text" placeholder='Username:' />
+      </form>
+    </div>
+  )
+}
+
+export default Search
