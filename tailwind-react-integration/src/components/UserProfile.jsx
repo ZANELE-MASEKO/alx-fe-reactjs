7b57c832e0531f0e88ff7e5e927c2e8036dc8064
zanele maseko <itszanele@gmail.com>
2025-11-28 17:02:39 +0200

    fixed image responsiveness

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,17 @@
 function UserProfile() {
   return (
     <div
-        class='bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg '>
-
-      <img src="https://via.placeholder.com/150" alt="User" class="bg-gray-100 
+        class="bg-gray-100 
                  p-4 sm:p-4 md:p-8 
                  max-w-xs sm:max-w-xs md:max-w-sm
                  mx-auto my-20 
-                 rounded-lg shadow-lg" />
+                 rounded-lg shadow-lg">
+
+      <img src="https://via.placeholder.com/150" alt="User" class="rounded-full 
+                   w-24 h-24 
+                   sm:w-24 sm:h-24 
+                   md:w-36 md:h-36 
+                   mx-auto"  />
 
       <h1
       class='text-xl text-blue-800 my-4 wm:text-lg md:text-xl text-center'>John Doe</h1>
