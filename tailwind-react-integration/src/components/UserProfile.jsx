f9cb37f1ec77e262d9521d0f3fb5c716e22589a1
zanele maseko <itszanele@gmail.com>
2025-11-28 16:50:47 +0200

    styled the user card

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,14 +1,17 @@
-function UserProfile (props) {
+function UserProfile() {
+  return (
+    <div
+        class='bg-gray-100 p-8 max-w-sm mx-auto, my-20 rounded-lg shadow-lg'>
 
-    return(
-        <div>
-            <h2>
-                {props.name}
-            </h2>
-            <p>Age: {props.age}</p>
-            <p>Bio: {props.bio}</p>
-        </div>
-    );
+      <img src="https://via.placeholder.com/150" alt="User" class='rounded-full w-36 h-36 mx-auto' />
+
+      <h1
+      class='text-xl text-blue-800 my-4'>John Doe</h1>
+
+      <p
+      class='text-gray-600 text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
+    </div>
+  );
 }
 
-export default UserProfile
+export default UserProfile;
