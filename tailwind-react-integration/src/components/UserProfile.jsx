6524ca019cd7d8a8af7a3f9980de7dc3d4529996
zanele maseko <itszanele@gmail.com>
2025-11-29 18:42:27 +0200

    updated userprofile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,23 +1,35 @@
 function UserProfile() {
   return (
     <div
-        class="bg-gray-100 
+      className="bg-gray-100 
                  p-4 sm:p-4 md:p-8 
                  max-w-xs sm:max-w-xs md:max-w-sm
                  mx-auto my-20 
-                 rounded-lg shadow-lg">
-
-      <img src="https://via.placeholder.com/150" alt="User" class="rounded-full 
+                 rounded-lg shadow-lg
+                 hover:shadow-xl transition-shadow duration-300"
+    >
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="rounded-full 
                    w-24 h-24 
                    sm:w-24 sm:h-24 
                    md:w-36 md:h-36 
-                   mx-auto"  />
+                   mx-auto
+                   hover:scale-110 
+                   transition-transform duration-300 ease-in-out"
+      />
 
       <h1
-      class='text-xl text-blue-800 my-4 wm:text-lg md:text-xl text-center'>John Doe</h1>
+        className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl text-center
+                   hover:text-blue-500 transition-colors duration-300"
+      >
+        John Doe
+      </h1>
 
-      <p
-      class='text-gray-600 text-base sm:text-sm md:text-base text-center'>Developer at Example Co. Loves to write code and explore new technologies.</p>
+      <p className="text-gray-600 text-base sm:text-sm md:text-base text-center">
+        Developer at Example Co. Loves to write code and explore new technologies.
+      </p>
     </div>
   );
 }
