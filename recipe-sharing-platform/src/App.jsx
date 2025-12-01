9e6d2f86a7854f269415021c620f8beee2c586dc
zanele maseko <itszanele@gmail.com>
2025-12-01 11:10:34 +0200

    tailwindcss

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import React from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 class='text-blue-500'> Tailwindcss</h1>
+    </>
+  )
+}
+
+export default App
